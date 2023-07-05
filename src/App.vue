e11601004ffe86a97beef1a122d8a6ef3aaf157c
Eding Muhamad <masaruedo@yahoo.com>
2023-07-06 04:26:12 +0700

    Create base Dropdown component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
-<script setup>
+<script>
+import Dropdown from './components/dropdown/Dropdown.vue';
+
+export default {
+  components: { Dropdown }
+}
 </script>
 
 <template>
-  <h1>Hello world!</h1>
+  <div class="center">
+    <Dropdown>
+      <template v-slot:trigger>
+        <button class="btn btn-secondary dropdown-toggle show" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Dropdown button
+        </button>
+      </template>
+      <template v-slot:menu>
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+      </template>
+    </Dropdown>
+  </div>
 </template>
 
 <style scoped>
-
+.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
 </style>
---
+++ src/components/dropdown/Dropdown.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="dropdown">
+      <slot name="trigger" />
+      <ul class="dropdown-menu show" :style="menuStyles">
+        <slot name="menu" />
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        menuStyles () {
+            return {
+                position: 'absolute', 
+                inset: '0px auto auto 0px',
+                margin: '0px',
+                transform: 'translate(0px, 40px)'
+            }
+        }
+    }
+}
+</script>
